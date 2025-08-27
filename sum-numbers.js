00c80f6d131ac6e04c91870337f862802390edb5
4nik <shak.iu0@gmail.com>
2025-08-28 00:00:25 +0600

    find sum of numbers

---
+++ sum-numbers.js
@@ -0,0 +1,11 @@
+function getSum(arr) {
+    let sum = 0;
+    for (let i=0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let total = getSum(numbers);
+console.log(total);
